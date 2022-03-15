2365986f066ead8fc9664029b1a3750bd80353d9
Halex Viotto Gomes <halexx002@gmail.com>
2022-03-15 16:22:30 -0400

    test: createFileStream should throw when createReadStream throws

--- tests/unit/server/service.test.js
+++ tests/unit/server/service.test.js
@@ -1,19 +1,33 @@
-import {describe, test, jest, expect} from '@jest/globals'
+import {describe, test, jest, expect, beforeEach} from '@jest/globals'
 import fs from 'fs'
 
 import {Service} from '../../../server/service.js'
 
 describe('#Service', () => {
+  
+  beforeEach(() => {
+    jest.restoreAllMocks()
+  })
+  
   describe('createFileStream', () => {
     test('it should call createReadStream with filename', () => {
       const sut = new Service()
       const expectedFilename = 'any'
 
-      const createReadStreamSpy = jest.spyOn(fs, fs.createReadStream.name).mockReturnValue()
+      const createReadStreamSpy = jest.spyOn(fs, fs.createReadStream.name).mockReturnValueOnce()
 
       sut.createFileStream(expectedFilename)
 
       expect(createReadStreamSpy).toHaveBeenCalledWith(expectedFilename)
+    })
+
+    test('it should throw when createReadStream throws', () => {
+      const sut = new Service()
+      const expectedFilename = 'any'
+
+      jest.spyOn(fs, fs.createReadStream.name).mockImplementationOnce(() => {throw new Error()})
+
+      expect(() => sut.createFileStream(expectedFilename)).toThrowError()
     })
   })
 })
