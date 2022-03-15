2c80d90387f04ee8a10c3fc84614d134a6fdc194
Halex Viotto Gomes <halexx002@gmail.com>
2022-03-15 10:38:28 -0400

    test: GET / - should redirect to home page

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,9 +1,12 @@
 import {jest, expect, describe, test, beforeEach} from '@jest/globals'
 
 import config from '../../../server/config.js'
+import { handler } from '../../../server/routes.js'
+import TestUtil from '../_util/testUtil.js'
 
 const {
-  pages
+  pages,
+  location
 } = config
 
 describe('#Routes - test suite for api response', () => {
@@ -12,7 +15,21 @@ describe('#Routes - test suite for api response', () => {
     jest.clearAllMocks()
   })
   
-  test.todo('GET / - should redirect to home page')
+  test('GET / - should redirect to home page', async () => {
+    const params = TestUtil.defaultHandleParams()
+    params.request.method = 'GET'
+    params.request.url = '/'
+
+    await handler(...params.values())
+
+    expect(params.response.writeHead).toBeCalledWith(
+      302,
+      {
+        'Location': location.home
+      }
+    )
+    expect(params.response.end).toHaveBeenCalled()
+  })
   test.todo(`GET /home - should response with ${pages.homeHTML} file stream`)
   test.todo(`GET /controller - should response with ${pages.controllerHTML} file stream`)
   test.todo(`GET /unknown - given a nonexistent route it should respond with 404`)
