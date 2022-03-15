ba28bb83c3ef87410008bd6db12739ebee9ab333
Halex Viotto Gomes <halexx002@gmail.com>
2022-03-15 09:39:00 -0400

    chore: add todo tests

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,7 +1,24 @@
-import {jest, expect, describe, test} from '@jest/globals'
+import {jest, expect, describe, test, beforeEach} from '@jest/globals'
 
-describe('#Routes', () => {
-  test('my test', () => {
-    expect(true).toBeTruthy()
+import config from '../../../server/config.js'
+
+const {
+  pages
+} = config
+
+describe('#Routes - test suite for api response', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks(),
+    jest.clearAllMocks()
+  })
+  
+  test.todo('GET / - should redirect to home page')
+  test.todo(`GET /home - should response with ${pages.homeHTML} file stream`)
+  test.todo(`GET /controller - should response with ${pages.controllerHTML} file stream`)
+  test.todo(`GET /unknown - given a nonexistent route it should respond with 404`)
+
+  describe('exceptions', () => {
+    test.todo('given a nonexistent file it should respond with 404')
+    test.todo('given an error file it should respond with 500')
   })
 })
